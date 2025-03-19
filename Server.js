c3bcdbb40624150d4e258fd4a404c738994d4ad6
Leimez <leevikempas@gmail.com>
2025-03-19 10:39:42 +0200

    3.2

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('json spaces', 2); 
+app.set('json spaces', 2); // Kaunistaa JSON-vastaukset
 
 const persons = [
   { id: 1, name: 'John Doe', number: '123-456-7890' },
@@ -13,6 +13,14 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+app.get('/info', (req, res) => {
+  const count = persons.length;
+  const date = new Date().toString();
+  res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
 });
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
