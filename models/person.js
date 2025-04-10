42fa122e632cad471b7ee5333d8e8be6c2b238d1
Leimez <leevikempas@gmail.com>
2025-04-10 13:38:32 +0300

    3.20

--- models/person.js
+++ models/person.js
@@ -8,7 +8,18 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+      },
+      message: props => `${props.value} is not a valid phone number! Number must be:
+- At least 8 characters long
+- Two parts separated by hyphen
+- First part: 2-3 digits
+- Second part: remaining digits
+Examples: 09-1234556 or 040-22334455`
+    }
   }
 });
 
